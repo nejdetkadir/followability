4e4628e500addf838db26a1834a2951b41bbb3aa
nejdetkadir <nejdetkadir.550@gmail.com>
2022-10-15 00:55:20 +0300

    Add new action for following

--- lib/followability/followable/actions/follow.rb
+++ lib/followability/followable/actions/follow.rb
@@ -6,7 +6,7 @@ module Followability
       module Follow
         def decline_follow_request_of(record); end
 
-        def remove_follow_request_of(record); end
+        def remove_follow_request_for(record); end
 
         def send_follow_request_to(record); end
 
@@ -15,6 +15,8 @@ module Followability
         def mutual_following_with?(record); end
 
         def sent_follow_request_to?(record); end
+
+        def follow_request_sent_by?(record); end
       end
     end
   end
