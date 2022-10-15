2a993b8366ba94c859d012a0c1079c00387d2ede
nejdetkadir <nejdetkadir.550@gmail.com>
2022-10-15 21:24:18 +0300

    Update readme

--- lib/followability/followable/actions/common.rb
+++ lib/followability/followable/actions/common.rb
@@ -4,18 +4,6 @@ module Followability
   module Followable
     module Actions
       module Common
-        def related_with_as_followerable?(record)
-          followerable_relationships.exists?(followable: record)
-        end
-
-        def related_with_as_followable?(record)
-          followable_relationships.exists?(followerable: record)
-        end
-
-        def related?(record)
-          related_with_as_followerable?(record) || related_with_as_followable?(record)
-        end
-
         def myself?(record)
           id == record.id
         end
